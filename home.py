c3da7d51b4155dbd3de0ec5d384ff7b43dfd5836
andrewsiah-abra <107221100+andrewsiah-abra@users.noreply.github.com>
2022-10-13 18:37:40 -0400

    added home page

--- home.py
+++ home.py
@@ -11,13 +11,21 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed")
 
-st.title("How Many Days since...")
+st.title("Iris-Andrew Anniversaries")
 
 date_we_kissed = date(2022, 9, 14)
 time_we_kissed = time(2, 00)
 dt_we_kissed = dt.datetime.combine(date_we_kissed, time_we_kissed)
+dt_im_bf = dt.datetime.combine(date(2022, 10, 13), time(7, 30))
+dt_now = datetime.now()
 
-st.write(dt_we_kissed)
+st.header(f"Date We Kissed 💏: ")
+st.subheader(dt_we_kissed)
+st.header(f"Days Since Kissed 💋: ")
+st.subheader((dt_now - dt_we_kissed).days)
 
-
+st.header(f"Date I'm your BF 🥰: ")
+st.subheader(dt_im_bf)
+st.header(f"Days since Andrew is boyfriend 💝: ")
+st.subheader((dt_now - dt_im_bf).days)
 
